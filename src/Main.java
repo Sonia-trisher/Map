6ad306c87dfa3b04c55a537550cfb9ca63b4c5fe
sonia-trisher <soniatrisher86@gmail.com>
2025-04-16 17:38:50 +0200

    adding method in map

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,32 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class Main {
     public static void main(String[] args) {
+        Map<Integer,String> students = new HashMap<>();
+//        StudentManager student = new StudentManager();
+//                students.put(1,"sonia");
+//                students.put(2,"Queen");
+//                students.put(3,"hope");
+        StudentManager.addStudent(students,5,"");
+        StudentManager.addStudent(students,4,"michael");
+        StudentManager.addStudent(students,3,"hope");
+        StudentManager.addStudent(students,2,"gaju");
+
+        StudentManager.sortStudents(students);
+
+
+
+        StudentManager.searchStudent(students,7);
+        StudentManager.searchStudent(students,4);
+
+        StudentManager.removeStudent(students,0);
+        StudentManager.removeStudent(students,2);
+
+        StudentManager.findStudentWithHighestId(students);
+        StudentManager.findStudentWithLowestId(students);
+
+
 
     }
 }
